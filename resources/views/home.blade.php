609385e40628627a003c8466db73eec47046b0ab
Bruno Gomes <brunogomes.dev@incibrasil.com.br>
2024-04-09 17:18:53 -0300

    breeze translation implemented

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,4 +14,64 @@
             </div>
         </div>
     </div>
+
+
+    <div>
+
+        <div class="flex justify-center">
+            <div x-data="{
+                open: false,
+                toggle() {
+                    if (this.open) {
+                        return this.close()
+                    }
+
+                    this.$refs.button.focus()
+
+                    this.open = true
+                },
+                close(focusAfter) {
+                    if (!this.open) return
+
+                    this.open = false
+
+                    focusAfter && focusAfter.focus()
+                }
+            }" x-on:keydown.escape.prevent.stop="close($refs.button)"
+                x-on:focusin.window="! $refs.panel.contains($event.target) && close()" x-id="['dropdown-button']"
+                class="relative">
+
+
+                <!-- Panel -->
+                <div
+                    x-placement="top-start"
+                    x-ref="panel"
+                    x-show="open"
+                    x-transition.origin.top.left
+                    x-on:click.outside="close($refs.button)"
+                    :id="$id('dropdown-button')" style="display: none;"
+                    class="absolute right-0 flex flex-col gap-2"
+                >
+                    <x-primary-button>
+                        Gasto
+                    </x-primary-button>
+                    <x-primary-button>
+                        Lista
+                    </x-primary-button>
+                </div>
+
+
+                <!-- Button -->
+                <button x-ref="button" x-on:click="toggle()" :aria-expanded="open"
+                    :aria-controls="$id('dropdown-button')" type="button"
+                    class="rounded-full inline-flex p-4 items-center bg-gray-800 dark:bg-gray-200 border border-transparent font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+
+    </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return redirect()->intended('home');
 });
 
 Route::get('/home', function () {
