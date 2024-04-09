34182a71ef379b9d6d424a54955e69fd629bbc92
Bruno Gomes <brunogomes.dev@incibrasil.com.br>
2024-04-09 19:18:15 -0300

    spendings relationships implemented

--- app/Models/Spending.php
+++ app/Models/Spending.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Spending extends Model
 {
     use HasFactory;
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
