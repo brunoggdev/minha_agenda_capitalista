c600fd8bb999c2f72003c1ec24865952933080a8
Bruno Gomes <brunogomes.dev@incibrasil.com.br>
2024-04-10 17:14:42 -0300

    added login with google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,7 +52,7 @@
     <a href="{{ url('/sign-in/google') }}"
         class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 w-full justify-around">
         <div class="flex items-center text-sm font-medium self-center">
-            <img class="max-h-7 object-contain"
+            <img class="max-h-5 object-contain"
                 src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA"
                 alt="google logo">
             <span class="ml-2.5">Google</span>
